df1fec1daf9d8bb37856beb6ac1443ea68f34c4f
Shinichiro Naito <github@shinichironaito.com>
2019-07-18 00:51:28 +0900

    update

--- app/main.py
+++ app/main.py
@@ -2,14 +2,39 @@ from datetime import datetime
 from time import sleep
 import schedule
 from ifttt import ifttt_webhook
+from scraping import get_status, get_open_lesson
+
+
+last_status = {
+    "tutor_book": None,
+    "reserve_day": None,
+    "exec_day": None
+}
 
 
 def job():
     print("do job at", datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-    payload = {"value1": 3,
-               "value2": ["21:00", "21:30", "23:00"]}
-    r = ifttt_webhook("open_lesson", payload=payload)
-    print(r.text)
+    lessons = None
+    today = datetime.today()
+    if last_status["tutor_book"] is not None and \
+        last_status["tutor_book"] == True and \
+        last_status["reserve_day"] == ("today" or "tomorrow") and \
+        last_status["exec_day"].day == today.day:
+        print("already booked")
+    else:
+        status = get_status()
+        if status is None:
+            exit(1)
+        driver, wait, (tutor_book, reserve_day, exec_day) = status
+        last_status["tutor_book"] = tutor_book
+        last_status["reserve_day"] = reserve_day
+        last_status["exec_day"] = exec_day
+        lessons = get_open_lesson(driver, wait)
+    if lessons is not None:
+        payload = {"value1": len(lessons),
+                   "value2": lessons}
+        r = ifttt_webhook("open_lesson", payload=payload)
+        print(r.text)
 
 
 def main():
--- app/scraping.py
+++ app/scraping.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.webdriver import WebElement
@@ -8,9 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from settings import *
 
 
-def scraping():
+def get_status():
     options = ChromeOptions()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = Chrome(options=options)
     wait = WebDriverWait(driver, 10)
@@ -23,15 +24,15 @@ def scraping():
         elif current_url == URL_MYPAGE:
             print("already login")
 
-        wait.until(EC.title_contains("マイページ"))
+        wait.until(EC.url_to_be(URL_MYPAGE))
 
-        check_lesson(driver, wait)
-    except WebDriverWait:
-        print("Error")
-        raise
-    finally:
+        return driver, wait, check_status(driver)
+
+    except WebDriverException as e:
+        print(e)
         driver.close()
         driver.quit()
+        return None
 
 
 def login(driver: Chrome, wait: WebDriverWait):
@@ -56,17 +57,79 @@ def login(driver: Chrome, wait: WebDriverWait):
         raise WebDriverException
 
 
-def check_lesson(driver: Chrome, wait: WebDriverWait):
+def check_status(driver: Chrome):
     try:
         element_lesson_list = driver.find_element(By.ID, ID_TIMELINE)
-        element_lesson_today = element_lesson_list.find_elements(
-            By.TAG_NAME, "p")
-        if len(element_lesson_today) != 0:
-            print(element_lesson_today[0].text)
+        element_lesson_days = element_lesson_list.find_elements(By.TAG_NAME, "p")
+        lesson_latest_day = datetime.strptime(
+            element_lesson_days[0].text, '%Y-%m-%d')
+        today = datetime.today()
+        if lesson_latest_day.day > today.day:
+            reserve_day = 'tomorrow'
+        elif lesson_latest_day.day == today.day:
+            reserve_day = 'today'
+        else:
+            reserve_day = 'past'
+        element_lesson_summary = element_lesson_list.find_elements(By.CLASS_NAME, CLASS_DAY)
+        element_latest_lesson = element_lesson_summary[0]
+        element_tutor_name = element_latest_lesson.find_element(By.CLASS_NAME, CLASS_TUTOR)
+        element_tutor_link = element_tutor_name.find_element(By.TAG_NAME, "a").get_attribute("href")
+        if element_tutor_link == URL_TUTOR:
+            tutor_book = True
+        else:
+            tutor_book = False
+        return tutor_book, reserve_day, today
+
+    except WebDriverException:
+        raise WebDriverException
+
+
+def get_open_lesson(driver: Chrome, wait: WebDriverWait):
+    try:
+        today = datetime.today()
+        lessons = get_day_open_lesson(driver, wait, today)
+        tomorrow = today + timedelta(days=1)
+        tomorrow_lessons = get_day_open_lesson(driver, wait, tomorrow)
+        for tomorrow_lesson in tomorrow_lessons:
+            lessons.append('tomorrow: ' + tomorrow_lesson)
+        if len(lessons) == 0:
+            return None
+        else:
+            return lessons
+
+    except WebDriverException as e:
+        print(e)
+        return None
+    finally:
+        driver.close()
+        driver.quit()
+
+
+def get_day_open_lesson(driver: Chrome, wait: WebDriverWait, day: datetime):
+    try:
+        day_url = URL_BOOKMARK + f"{day.year}/{day.month}/{day.day}/#page=1"
+        driver.get(day_url)
+        wait.until(EC.url_to_be(day_url))
+        wait.until(EC.text_to_be_present_in_element(
+            (By.CLASS_NAME, CLASS_RESULT_TTL),
+            f"{day.year}年{day.month}月{day.day}日の検索結果"))
+        element_tutor = driver.find_element(By.ID, ID_TUTOR)
+        element_open_lesson_div = element_tutor.find_element(By.CLASS_NAME, CLASS_OPEN_LESSON)
+        elements_open_lesson = element_open_lesson_div.find_elements(By.TAG_NAME, "a")
+        lessons = list()
+        if len(elements_open_lesson) != 0:
+            for element_open_lesson in elements_open_lesson:
+                lessons.append(element_open_lesson.text)
+        return lessons
 
     except WebDriverException:
         raise WebDriverException
 
 
 if __name__ == "__main__":
-    scraping()
+    driver, wait, (tutor_book, reserve_day, today) = get_status()
+    print(tutor_book)
+    print(reserve_day)
+    print(today)
+    lessons = get_open_lesson(driver, wait)
+    print(lessons)
